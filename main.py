8560c93d6dc0ed78815e15356428d6bee641cbe7
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-01-08 01:38:04 +0200

    Open browser automatically

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import webbrowser
+import threading
 from app import app
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  threading.Timer(1, webbrowser.open_new("http://127.0.0.1:5000/"))
+  app.run(port=5000)
